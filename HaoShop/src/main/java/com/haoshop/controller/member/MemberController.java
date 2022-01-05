3715eedfbb049aa60dc6c368ff5896c8de5208c0
mrhi04-18 <mrhi04-18@DESKTOP-AGKT4CG>
2022-01-05 13:12:11 +0900

    commit

--- HaoShop/src/main/java/com/haoshop/controller/member/MemberController.java
+++ HaoShop/src/main/java/com/haoshop/controller/member/MemberController.java
@@ -1,4 +1,4 @@
-package com.haoshop.view.member;
+package com.haoshop.controller.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
